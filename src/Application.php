3b57b604d792fc2b26cf11b56e3b245309d47490
Daryl <daryl@clubdeuce.net>
2025-12-08 22:07:03 -0500

    Extend `Application` class from `Base` and implement DI container handling

--- src/Application.php
+++ src/Application.php
@@ -2,15 +2,22 @@
 
 namespace Clubdeuce\Wpmvc_Redux;
 
+use Clubdeuce\Wpmvc_Redux\Base\Base;
+use DI\Container;
+
 /**
  * Base class for the WP MVC Redux library
  */
-class Application
+class Application extends Base
 {
     /**
      * Application version
      */
     const string VERSION = '1.0.0';
+
+    public function __construct(protected Container $container)
+    {
+    }
 
     /**
      * Get the library version
@@ -21,4 +28,17 @@ class Application
     {
         return self::VERSION;
     }
+
+    public function container(): Container
+    {
+        return $this->container;
+    }
+
+    public function setContainer(Container $container): self
+    {
+        $this->container  = $container;
+
+        return $this;
+    }
+
 }
