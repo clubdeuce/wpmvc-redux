ffab5ee98518855e3f36532703841108a1a5c8f5
Daryl <daryl@clubdeuce.net>
2026-03-03 14:29:00 -0500

    Introduce `Taxonomy` controller class to handle taxonomy registration and initialization

---
+++ src/Controllers/Taxonomy.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Clubdeuce\Wpmvc_Redux\Controllers;
+
+use Clubdeuce\Wpmvc_Redux\Base\Base;
+
+class Taxonomy extends Base {
+
+	const TAXONOMY = null;
+
+	protected array $object_type = array();
+
+	protected array $arguments = array();
+
+	public function __construct( array $args = array() ) {
+
+		$this->set_state( $args );
+		$this->register_actions();
+
+	}
+
+	protected function register_actions(): void {
+
+		add_action( 'init', array( $this, 'init' ) );
+
+	}
+
+	public function init(): void {
+
+		$this->register_taxonomy();
+
+	}
+
+	public function arguments(): array {
+
+		return array_merge( [], $this->arguments );
+
+	}
+
+	public function object_type(): array {
+
+		return $this->object_type;
+
+	}
+
+	/**
+	 * Register the taxonomy
+	 *
+	 * @return void
+	 */
+	public function register_taxonomy(): void {
+
+		register_taxonomy( static::TAXONOMY, $this->object_type(), $this->arguments() );
+
+	}
+
+}
